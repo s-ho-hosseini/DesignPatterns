842d2f6b230662b7f02d48d6a2e3f72c5271b027
amirhossein <h-hosseini@hotmail.com>
2021-12-05 10:29:15 +0330

    Facade pattern added

--- Structural/Facade/Program.cs
+++ Structural/Facade/Program.cs
@@ -1,3 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+namespace Facade
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var service = new NotificationService();
+            service.Send("","");
+        }
+
+        public class Message
+        {
+            private string _contet;
+
+            public Message(string contet)
+            {
+                _contet = contet;
+            }
+        }
+
+        public class NotificationService
+        {
+            public void Send(string message, string taregt)
+            {
+                var server = new NotificationServer();
+                var connection = server.Connect("ip");
+                var authToken = server.Authentication("appId", "key");
+                server.Send(authToken, new Message(message), taregt);
+                connection.Disconnect();
+            }
+        }
+
+        public class NotificationServer
+        {
+            public Connection Connect(string ipAddress)
+            {
+                return new Connection();
+            }
+
+            public AuthToken Authentication(string appId, string key)
+            {
+                return new AuthToken();
+            }
+
+            public void Send(AuthToken authToken, Message message, string target)
+            {
+                Console.WriteLine("Sending a message");
+            }
+        }
+
+        public class Connection
+        {
+            public void Disconnect()
+            {
+            }
+        }
+
+        public class AuthToken
+        {
+        }
+    }
+}
